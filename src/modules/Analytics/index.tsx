ea29b7241edafdd1e8d09c9fefde94a270274c09
LBA-Lois <loisomodibo@gmail.com>
2024-08-16 17:42:04 +0100

    new

--- src/modules/Analytics/index.tsx
+++ src/modules/Analytics/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import PrivateLayout from "../../layout/PrivateLayout";
+import ComingSoon from "../ComingSoon/ComingSoon";
 
 const Analytics = () => {
-  return <div></div>;
+  return (
+    <PrivateLayout
+      pageTitle="Analytics"
+      name="Overview"
+      role="user"
+      pageName="overview"
+    >
+      <ComingSoon />
+    </PrivateLayout>
+  );
 };
 
 export default Analytics;
